6f9ffa3604e72b115844983741e9360716ff8b30
Ilyas Murtazin <vancs@mail.ru>
2026-05-28 12:04:20 +0300

    task 3 done

--- main.tf
+++ main.tf
@@ -38,3 +38,39 @@ resource "yandex_compute_instance" "platform" {
   }
 
 }
+
+resource "yandex_vpc_subnet" "db" {
+  name           = var.vm_db_vpc_name
+  zone           = var.vm_db_zone
+  network_id     = yandex_vpc_network.develop.id
+  v4_cidr_blocks = var.vm_db_cidr
+}
+
+resource "yandex_compute_instance" "platform_db" {
+  name        = var.vm_db_name
+  platform_id = var.vm_db_platform_id
+  zone        = var.vm_db_zone
+  resources {
+    cores         = var.vm_db_resources.cores
+    memory        = var.vm_db_resources.memory
+    core_fraction = var.vm_db_resources.core_fraction
+  }
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu.image_id
+    }
+  }
+  scheduling_policy {
+    preemptible = true
+  }
+  network_interface {
+    subnet_id = yandex_vpc_subnet.db.id
+    nat       = true
+  }
+
+  metadata = {
+    serial-port-enable = 1
+    ssh-keys           = "ubuntu:${var.vms_ssh_root_key}"
+  }
+
+}
---
+++ vms_platform.tf
@@ -0,0 +1,34 @@
+variable "vm_db_name" {
+  type        = string
+  default     = "netology-develop-platform-db"
+}
+
+variable "vm_db_platform_id" {
+  type        = string
+  default     = "standard-v4a"
+}
+
+variable "vm_db_resources" {
+  type = map(number)
+  default = {
+    cores         = 2
+    memory        = 2
+    core_fraction = 20
+  }
+}
+
+variable "vm_db_zone" {
+  type        = string
+  default     = "ru-central1-b"
+}
+
+variable "vm_db_vpc_name" {
+  type        = string
+  default     = "db"
+  description = "VPC network & subnet name"
+}
+
+variable "vm_db_cidr" {
+  type        = list(string)
+  default     = ["10.0.2.0/24"]
+}
